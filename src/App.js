1015909d22722d44de45dcabb5da7c6070ff7ce6
Fox,Mathijs M <m.fox@student.fontys.nl>
2020-05-06 17:33:38 +0200

    routing and login/register works now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
-import Login from './pages/login/Login'
-import Register from './pages/login/Register'
-import Adverts from './pages/adverts/Adverts'
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import * as firebase from "firebase/app";
 import "firebase/auth";
-import nav from './components/nav'
+import Nav from './components/nav'
 
 function App() {
 
@@ -25,28 +21,8 @@ function App() {
 
   firebase.initializeApp(firebaseConfig);
 
-  var content = <div className="form-inline my-2 my-lg-0">
-    <Link to="/Register">
-      <button className="btn btn-outline-success my-2 my-sm-0" style={{ marginRight: 10 }}>Register</button>
-    </Link>
-    <Link to="/Login">
-      <button className="btn btn-outline-success my-2 my-sm-0">Login</button>
-    </Link>
-  </div>
-
-  
-
   return (
-    <div className="container-fluid">
-      <nav> </nav>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Adverts} />
-          <Route exact path="/Login" component={Login} />
-          <Route exact path="/Register" component={Register} />
-        </Switch>
-      </Router>
-    </div>
+    <Nav />
   );
 }
 
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,64 +1,88 @@
 import React, { Component } from 'react';
+import Login from '../pages/login/Login';
+import Register from '../pages/login/Register';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Adverts from '../pages/adverts/Adverts';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
 class nav extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            content = null
+            content: null
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         firebase.auth().onAuthStateChanged(firebaseUser => {
             if (firebaseUser) {
-              console.log("hij komt hier wel rick")
-              content = <button className="btn btn-outline-success my-2 my-sm-0" style={{ marginRight: 10 }}>log out</button>
-        
+                console.log(firebaseUser);
+                this.setState({ content: <button className="btn btn-outline-success my-2 my-sm-0" onClick={this.logout} style={{ marginRight: 10 }}>log out</button> });
             }
             else {
-              content = <div className="form-inline my-2 my-lg-0">
-                <Link to="/Register">
-                  <button className="btn btn-outline-success my-2 my-sm-0" style={{ marginRight: 10 }}>Register</button>
-                </Link>
-                <Link to="/Login">
-                  <button className="btn btn-outline-success my-2 my-sm-0">Login</button>
-                </Link>
-              </div>
+                this.setState({
+                    content: <div className="form-inline my-2 my-lg-0">
+                        <Link to="/Register">
+                            <button className="btn btn-outline-success my-2 my-sm-0" style={{ marginRight: 10 }}>Register</button>
+                        </Link>
+                        <Link to="/Login">
+                            <button className="btn btn-outline-success my-2 my-sm-0">Login</button>
+                        </Link>
+                    </div>
+
+                })
             }
-          })
+        })
     }
-    
+
+    logout() {
+        firebase.auth().signOut();
+    }
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to="/">
-                    <div className="navbar-brand">Expplore</div>
-                </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+            <div className="container-fluid">
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <div className="nav-link">Home <span className="sr-only">(current)</span></div>
-                        </li>
-                        <li className="nav-item">
+                <Router>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-                        </li>
-                        <li className="nav-item dropdown">
-                            <div className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</div>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <Link to="/">
+                            <div className="navbar-brand">Expplore</div>
+                        </Link>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
 
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <div className="nav-link">Home <span className="sr-only">(current)</span></div>
+                                </li>
+                                <li className="nav-item">
+
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <div className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</div>
+                                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    </div>
+                                </li>
+                            </ul>
+                            <div>
+                                {this.state.content}
                             </div>
-                        </li>
-                    </ul>
-                    <div>
-                        {content}
-                    </div>
-                </div>
-            </nav>
+                        </div>
+
+
+                    </nav>
+                    <Switch>
+                        <Route exact path="/" component={Adverts} />
+                        <Route exact path="/Login" component={Login} />
+                        <Route exact path="/Register" component={Register} />
+                    </Switch>
+                </Router>
+            </div>
         );
     }
 }
